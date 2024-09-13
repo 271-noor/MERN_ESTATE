fbf56bd70a2b30cead482edadf1880e7edbf25ce
271-noor <157873714+271-noor@users.noreply.github.com>
2024-09-13 17:33:36 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,41 @@
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 // import { errorHandler } from "../utils/error.js";
 
+// For Sign Up...
 export const signup = async (req, res, next) => {
-
-const {username, email, password} = req.body;
-const hashedPassword = bcryptjs.hashSync(password, 10);
-const newUser = new User({username, email, password: hashedPassword});
-try {
-    await newUser.save()
-    res.status(201).json('User Created Successfully!');
-   
-} catch (error) {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Successfully!");
+  } catch (error) {
     next(error);
-}
-}
+  }
+};
+
+// For Sign In...
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // check the Email valid or not!
+    const validUser = await User.findOne({ email });
+
+    // Condition For Email is not Valid...
+    if (!validUser) return next(errorHandler(404, "User Not Found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest }= validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
