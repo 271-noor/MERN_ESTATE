8b4a9a0ab19219defa8c6bce9a84b5837e15996b
271-noor <157873714+271-noor@users.noreply.github.com>
2024-09-13 16:38:19 +0530

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs'
-import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs'
+// import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
 
 const {username, email, password} = req.body;
-const hashedPassword = bcrypt.hashSync(password, 10);
+const hashedPassword = bcryptjs.hashSync(password, 10);
 const newUser = new User({username, email, password: hashedPassword});
 try {
     await newUser.save()
