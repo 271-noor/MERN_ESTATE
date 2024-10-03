8b4d8f78e2e82339592a8eff15a15c0540ddab1e
271-noor <157873714+271-noor@users.noreply.github.com>
2024-10-03 22:07:45 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { creatListing, deleteListing, getListing, updateListing } from "../controllers/listing.controller.js";
+import { creatListing, deleteListing, getListing, getListings, updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, creatListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
 router.post('/get/:id', getListing)
+router.post('/get', getListings)
 
 export default router;
